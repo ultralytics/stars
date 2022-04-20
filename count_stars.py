4f2ac558891d6f52b0d23ade9bb416ae31aadd3f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-19 21:52:26 -0700

    Add argparser

--- count_stars.py
+++ count_stars.py
@@ -6,6 +6,7 @@ Usage:
     $ python path/to/count_stars.py
 """
 
+import argparse
 import time
 from datetime import datetime
 
@@ -13,97 +14,118 @@ import pandas as pd
 from github import Github  # pip install PyGithub
 from tqdm import tqdm
 
-# Settings
-TOKEN = "ghp_YEKVmKSn1Z9..."  # GitHub access token
-# date = datetime(2022, 3, 1)  # count stars since this day, i.e. March 1st 2022
-# days = (datetime.now() - date).total_seconds() / 86400  # compute number of days
-days = 30  # specify days directly, i.e. last 30 days
 
-repos = [
-    'ultralytics/yolov5',  # YOLOv5 🚀
+def run(
+        token="ghp_YEKVmKSn1Z9...",  # GitHub access token
+        days=30,  # trailing days to analyze
+):
+    # Settings
+    # date = datetime(2022, 3, 1)  # count stars since this day, i.e. March 1st 2022
+    # days = (datetime.now() - date).total_seconds() / 86400  # compute number of days
+    # days = 30  # specify days directly, i.e. last 30 days
 
-    'facebookresearch/detectron2',  # FAANG companies
-    'deepmind/deepmind-research',
-    'aws/amazon-sagemaker-examples',
-    'awslabs/autogluon',
-    'microsoft/lightgbm',
-    'openai/gpt-3',
-    'apple/turicreate',
-    'apple/coremltools',
-    'google/automl',
-    'google-research/google-research',
-    'google-research/vision_transformer',
-    'google-research/bert',
-    'NVlabs/stylegan3',
+    repos = [
+        'ultralytics/yolov5',  # YOLOv5 🚀
 
-    'tencent/ncnn',  # Chinese companies
-    'Megvii-BaseDetection/YOLOX',
-    'PaddlePaddle/Paddle',
+        'facebookresearch/detectron2',  # FAANG companies
+        'deepmind/deepmind-research',
+        'aws/amazon-sagemaker-examples',
+        'awslabs/autogluon',
+        'microsoft/lightgbm',
+        'openai/gpt-3',
+        'apple/turicreate',
+        'apple/coremltools',
+        'google/automl',
+        'google-research/google-research',
+        'google-research/vision_transformer',
+        'google-research/bert',
+        'NVlabs/stylegan3',
 
-    'rwightman/pytorch-image-models',  # Startups/architectures
-    'streamlit/streamlit',
-    'explosion/spaCy',
-    'PyTorchLightning/pytorch-lightning',
-    'ray-project/ray',
-    'fastai/fastai',
-    'alexeyab/darknet',
-    'pjreddie/darknet',
-    'WongKinYiu/yolor',
-    'wandb/client',
-    'Deci-AI/super-gradients',
-    'neuralmagic/sparseml',
-    'MosaicML/composer',
-    'nebuly-ai/nebullvm',
-    # edgeimpulse  # not open-source
-    # octoml  # not open-source
-    # 'huggingface/transformers',  # known issue over 40k stars https://github.com/PyGithub/PyGithub/issues/1876
-]
-save = False  # save user info
-sleep = 1.5  # sleep time between requests
+        'tencent/ncnn',  # Chinese companies
+        'Megvii-BaseDetection/YOLOX',
+        'PaddlePaddle/Paddle',
 
-# Parameters
-g = Github(TOKEN)  # create a Github instance
-print(f'Counting stars for last {days:.1f} days from {datetime.now():%d %B %Y}\n')
-pd.options.display.max_columns = None
+        'rwightman/pytorch-image-models',  # Startups/architectures
+        'streamlit/streamlit',
+        'explosion/spaCy',
+        'PyTorchLightning/pytorch-lightning',
+        'ray-project/ray',
+        'fastai/fastai',
+        'alexeyab/darknet',
+        'pjreddie/darknet',
+        'WongKinYiu/yolor',
+        'wandb/client',
+        'Deci-AI/super-gradients',
+        'neuralmagic/sparseml',
+        'MosaicML/composer',
+        'nebuly-ai/nebullvm',
+        # edgeimpulse  # not open-source
+        # octoml  # not open-source
+        # 'huggingface/transformers',  # known issue over 40k stars https://github.com/PyGithub/PyGithub/issues/1876
+    ]
+    save = False  # save user info
+    sleep = 1.5  # sleep time between requests
 
-# Run
-t, users = time.time(), []
-for repo in repos:
-    r = g.get_repo(repo)
-    s = r.get_stargazers_with_dates().reversed
-    total = s.totalCount
+    # Parameters
+    g = Github(token)  # create a Github instance
+    print(f'Counting stars for last {days:.1f} days from {datetime.now():%d %B %Y}\n')
+    pd.options.display.max_columns = None
 
-    dates = []
-    try:
-        pbar = tqdm(s, total=total, desc=r.full_name)
-        for x in pbar:
-            dt = (datetime.now() - x.starred_at).total_seconds() / 86400
-            if dt < days:
-                dates.append([x.starred_at, 1])
-                if save:
-                    time.sleep(sleep)
-                    u = x.user
-                    if u.email is not None:
-                        users.append([r.full_name, u.name, u.company, u.email, u.location, u.html_url, u.followers,
-                                      x.starred_at])
-            else:
-                n = len(dates)
-                s1 = f'{n} stars'
-                s2 = f'({n / days:.1f}/day)'
-                pbar.desc = f'{r.full_name:40s}{s1:12s}{s2:12s}'
-                break
+    # Run
+    t, users = time.time(), []
+    for repo in repos:
+        r = g.get_repo(repo)
+        s = r.get_stargazers_with_dates().reversed
+        total = s.totalCount
 
-    except Exception as e:
-        print(e)
+        dates = []
+        try:
+            pbar = tqdm(s, total=total, desc=r.full_name)
+            for x in pbar:
+                dt = (datetime.now() - x.starred_at).total_seconds() / 86400
+                if dt < days:
+                    dates.append([x.starred_at, 1])
+                    if save:
+                        time.sleep(sleep)
+                        u = x.user
+                        if u.email is not None:
+                            users.append([r.full_name, u.name, u.company, u.email, u.location, u.html_url, u.followers,
+                                          x.starred_at])
+                else:
+                    n = len(dates)
+                    s1 = f'{n} stars'
+                    s2 = f'({n / days:.1f}/day)'
+                    pbar.desc = f'{r.full_name:40s}{s1:12s}{s2:12s}'
+                    break
 
-    df = pd.DataFrame(dates, columns=['date', 'stars'])
-    df.date = pd.to_datetime(df.date)
-    # df.to_csv(f'dates_{r.name}.csv')
-    # dg = df.groupby(pd.Grouper(key='date', freq='1M')).sum()  # group by month
-    # dg = df.groupby(pd.Grouper(key='date', freq='1D')).sum()  # group by day
+        except Exception as e:
+            print(e)
 
-print(f'Done in {time.time() - t:.1f}s')
-if save:
-    x = pd.DataFrame(users, columns=['Repo', 'Name', 'Company', 'Email', 'Location', 'GitHub', 'Followers', 'Date'])
-    x.to_csv(f'users.csv')
-    print(f'{len(x)} users saved to users.csv')
+        df = pd.DataFrame(dates, columns=['date', 'stars'])
+        df.date = pd.to_datetime(df.date)
+        # df.to_csv(f'dates_{r.name}.csv')
+        # dg = df.groupby(pd.Grouper(key='date', freq='1M')).sum()  # group by month
+        # dg = df.groupby(pd.Grouper(key='date', freq='1D')).sum()  # group by day
+
+    print(f'Done in {time.time() - t:.1f}s')
+    if save:
+        x = pd.DataFrame(users, columns=['Repo', 'Name', 'Company', 'Email', 'Location', 'GitHub', 'Followers', 'Date'])
+        x.to_csv(f'users.csv')
+        print(f'{len(x)} users saved to users.csv')
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', type=str, default='ghp_YEKVmKSn1Z9...', help='GitHub access token')
+    parser.add_argument('--days', type=int, default=30, help='Trailing days to analyze')
+    opt = parser.parse_args()
+    return opt
+
+
+def main(opt):
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
