80e29c32c605bbd385ac5286d8ed59a3c63c3aac
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-07-05 14:31:51 +0200

    Add Discourse at https://community.ultralytics.com (#31)

--- count_stars.py
+++ count_stars.py
@@ -24,6 +24,7 @@ def run(
     days=30,  # trailing days to analyze
     save=False,  # save user info
 ):
+    """Counts GitHub stars for specified repositories over a given period and optionally saves user information."""
     # Settings
     # date = datetime(2022, 3, 1)  # count stars since this day, i.e. March 1st 2022
     # days = (datetime.now() - date).total_seconds() / 86400  # compute number of days
